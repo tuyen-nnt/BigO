100a040b0730599da2965fc45155268245bc2231
Tuyen Nguyen <contact@tuyen.me>
2020-11-24 00:13:53 +0700

    Intro to Algorithms

---
+++ Lession1/TongLuong/cmake-build-debug/CMakeFiles/TongLuong.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TongLuong.dir/main.cpp.o"
+  "TongLuong"
+  "TongLuong.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TongLuong.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
